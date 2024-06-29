62bbfe6cd619fde2ab0626d9fd98c72f1c2572f8
Yan Feng <y77feng@uwaterloo.ca>
2024-06-29 08:53:24 +0000

    add logging

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import logging
 import traceback
 
 import numpy as np
@@ -5,6 +6,10 @@ from PIL import Image
 from flask import Flask, request, jsonify
 
 from AI_Python.model_inferencing import ClassificationModelInference
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -25,8 +30,8 @@ def predict_bag():
             img = img.resize((224, 224))
             result = bag_classification_model.model_predict_main(np.array(img.convert('RGB')), 5)
             return jsonify(result)
-        except:
-            print(traceback.format_exc())
+        except Exception as error:
+            logger.exception(error)
             return jsonify({'message': 'Failed to recognize the image'}), 500
 
     return jsonify({'message': 'Only POST method allowed'}), 400
