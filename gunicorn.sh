a9eea55c349a687d207a4ca6ec5cad352595a51f
Yan Feng <y77feng@uwaterloo.ca>
2024-06-29 22:49:10 +0800

    fix shell type

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-source ./bin/activate
+source bin/activate
 
 nohup gunicorn --chdir app wsgi:app -w 2 --threads 2 -b 0.0.0.0:5000 > brand-classification.log 2>&1&
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 pip3 install -r requirements-torch.txt --extra-index-url https://download.pytorch.org/whl/cpu
 pip3 install -r requirements.txt
