a46df61d2a9a3211c5ee9a03afd14e2bd7ebd0b7
Yan Feng <y77feng@uwaterloo.ca>
2024-06-29 22:36:30 +0800

    remove kill

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ps aux | grep gunicorn | awk '{print $2}' | xargs sudo kill -9
-
 source ./bin/activate
 
 nohup gunicorn --chdir app wsgi:app -w 2 --threads 2 -b 0.0.0.0:5000 > brand-classification.log 2>&1&
